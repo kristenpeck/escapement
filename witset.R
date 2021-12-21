3a0a4c4db8994fbe44ab95d9f2aa888a293abfa8
kristen.peck <kristen.peck@dfo-mpo.gc.ca>
2021-12-20 17:26:55 -0800

    Updated QA, added LP estimate for SK

--- witset.R
+++ witset.R
@@ -58,7 +58,7 @@ SKbylocation
 #QA:
 
 #note that the column Recaptured Color has 7 numbers in there, not just colors (mostly 2017)
-#   To fix manually!
+#   To fix manually!- DONE
 
 #how many fish recapped at campground?
 witsetSK %>% 
@@ -80,38 +80,59 @@ witsetSK %>%
 
 
 
-#are there any duplicate new tag numbers within year? Yes
-#   Does colour need to be attached? Yes - done
-anyDuplicated(witsetSK$newtags)
+#are there any duplicate new tag numbers within 2021? Yes
+#   Does colour need to be attached? Yes - done with new.tag column
 
+tmp <- witsetSK %>% 
+  filter(!is.na(new.tag), year %in% 2021) 
+tmp[duplicated(tmp$new.tag),] %>% 
+  select(Sample_Date, Location_Code, Counter, new.tag)
+#2021: y-4901 duplicate not fixable, the rest fixed
 
-#any tags misrecorded as a new tag that should be a recap? (will need to 
-# order the tags by date, then check each line if self is found in
-# any preceding line)
 
 #how many recaptures without a new tag record? Filter out.
 (new.recaps <- witsetSK %>% 
   filter(year %in% 2021) %>% 
   filter(!is.na(recap.tag), 
-         !(recap.tag %in% new.tag) ) )
+         !(recap.tag %in% new.tag) ) ) %>% 
+  select(Sample_Date, Location_Code, Counter, recap.tag)
 #there are 15 recaptures without a matching tag number in 2021
+#canyon data not entered for SK on July 19th 2021- DONE
+#need to fix database structure for A2 tags (so they don't get put in recaps)
 
 #how many new tag numbers with no colour?
 witsetSK %>% 
-  filter(!is.na(AppliedTagNumber),is.na(AppliedColor))
-#77 missing colour of tag
+  filter(!is.na(AppliedTagNumber),is.na(AppliedColor), year %in% 2021)
+#77 missing colour of tag, just 1 in 2021 - DONE
 
 #how many tag colours with no tag number?
 witsetSK %>% 
-  filter(!is.na(AppliedColor),is.na(AppliedTagNumber))
+  filter(!is.na(AppliedColor),is.na(AppliedTagNumber), year %in% 2021)
 #160 tags with no number, just a colour
+
+#how many new tag numbers with no colour?
+witsetSK %>% 
+  filter(!is.na(AppliedTagNumber),is.na(AppliedColor), year %in% 2021)
+#77 missing colour of tag, just 1 in 2021 - DONE
+
+#how many recap tag colours with no tag number?
+witsetSK %>% 
+  filter(!is.na(`Recaptured Color`),is.na(`Recaptured number`), year %in% 2021)
+#160 tags with no number, just a colour; 3 in 2021, all b/c recap tag number doesn't exist
+
+#how many recap tag numbers with no colour?
+witsetSK %>% 
+  filter(!is.na(`Recaptured number`),is.na(`Recaptured Color`), year %in% 2021)
+#none in 2021
 
 #check if the tag status is recorded incorrectly
 (no.tag.number <- witsetSK %>% 
   filter(TagStatus %in% c("A","A2"),
-         is.na(AppliedTagNumber)&is.na(AppliedColor)))
+         is.na(AppliedTagNumber)&is.na(AppliedColor))) %>% 
+  filter(year %in% 2021) %>% 
+  select(Counter)
 #49 cases where tag status was recorded as a new tag but no tag number 
-# or colour
+# or colour. Just 1 in 2021- fixed
 
 
   
@@ -119,45 +140,83 @@ witsetSK %>%
 
 #get closed LP estimate from fish tagged at the campground, 
 # then recaptured at the canyon. Assume no tags lost (i.e. no fallback)
+#year.select <- 2017
 
-markedcampground21 <- witsetSK %>% 
-  filter(year %in% 2021) %>% 
+markedcampground <- witsetSK %>% 
+  #filter(year %in% year.select) %>% 
   filter(Location_Code %in% "Campground") %>% 
   filter(TagStatus %in% c("A","A2")) 
-length(unique(markedcampground21$new.tag))
+#length(unique(markedcampground$new.tag))
 
-markedcampground21[anyDuplicated(markedcampground21$new.tag),"new.tag"]
-#561 fish marked at campground in 2021 
-#one duplicate new tag put out: yellow 5648 on Sept 1st, 2021. One was a VHF
+#markedcampground[anyDuplicated(markedcampground$new.tag),"new.tag"]
+#562 fish marked at campground in 2021 
+
 
 # # of new tags put out in the canyon
 newtagscanyon <- witsetSK %>% 
-  filter(year %in% 2021) %>% 
+  #filter(year %in% year.select) %>% 
   filter(Location_Code %in% "Canyon") %>% 
   #filter(TagStatus %in% c("A","A2")) %>% 
   filter(!is.na(new.tag))
-length(unique(newtagscanyon$new.tag))
+#length(unique(newtagscanyon$new.tag))
 
-newtagscanyon[anyDuplicated(newtagscanyon$new.tag),"new.tag"]
-#1773 fish marked at canyon in 2021
-# one duplicate new tag put out: yellow 4901 on 27 July 2021. Already noted
+#newtagscanyon[anyDuplicated(newtagscanyon$new.tag),"new.tag"]
+#1788 fish marked at canyon in 2021
+# one duplicate new tag put out: yellow 4901 on 27 July 2021. Already noted in db
 
 
 # filter out new canyon tags from recaps at canyon
 canyontagrecaps <- witsetSK %>% 
-  filter(year %in% 2021) %>% 
+  #filter(year %in% year.select) %>% 
   filter(Location_Code %in% "Canyon") %>% 
   filter(TagStatus %in% c("AR","R")) %>% 
   filter(!(recap.tag %in% unique(newtagscanyon$new.tag)))
-#53 fish recapped at canyon from campground in 2021
+#49 fish recapped at canyon from campground in 2021
 
+#total canyon catch excluding canyon tags
 canyoncatch <- witsetSK %>% 
-  filter(year %in% 2021) %>% 
-  filter(Location_Code %in% "Canyon") %>% 
+  #filter(year %in% year.select) %>% 
+  filter(Location_Code %in% "Canyon") 
+%>% 
   filter(!(recap.tag %in% unique(newtagscanyon$new.tag)))
-
+#2452 SK captured at canyon. Should exclude other portions of catch?
   
-length(unique(markedcampground21$AppliedTagNumber))
+
+#LP estimate for campground -> canyon
+
+#marked in campground
+(m <- markedcampground %>% 
+  group_by(year) %>% 
+  summarize(marked = length(Sample_Date)))
+
+#recapped at canyon
+(r <- canyontagrecaps %>% 
+    group_by(year) %>% 
+    summarize(recapped = length(Sample_Date)))
+
+#total unique fish caught at canyon
+(c <- canyoncatch %>% 
+    group_by(year) %>% 
+    summarize(total.catch = length(Sample_Date)))
+
+
+LP <- m %>% 
+  left_join(r) %>% 
+  left_join(c) %>% 
+  mutate(LP = marked*total.catch/recapped)
+LP
+
+ggplot(data=LP)+
+  geom_line(aes(x=year, y=LP))+
+  geom_point(aes(x=year, y=LP))+
+  labs(y="LP estimated population size (SK)")
+
+
+
+
+
+
+
 
 
 
